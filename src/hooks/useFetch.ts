f1ac314d2da71747291be4e1fa9bfca9dc1713c7
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-04-21 10:12:23 +0100

    Refactor nation.

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,7 +4,7 @@ import { client } from "lib/client";
 
 interface UseFetchResponse {
   fetchData: <T>(endpoint: string) => Promise<T | undefined>;
-  error?: string | undefined;
+  error?: string;
 }
 
 export function useFetch(): UseFetchResponse {
@@ -18,11 +18,11 @@ export function useFetch(): UseFetchResponse {
 
       return data;
     } catch (error: any | AxiosError) {
-      if (axios.isAxiosError(error) && error.response) {
-        if (error.response.status === 401) {
-          const refreshed = await refreshAccessToken();
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          const isRefreshed = await refreshAccessToken();
 
-          if (!refreshed) {
+          if (!isRefreshed) {
             setError(
               "Your session has expired, please refresh the page for a new session."
             );
@@ -40,18 +40,18 @@ export function useFetch(): UseFetchResponse {
     }
   }
 
-  async function refreshAccessToken(): Promise<boolean> {
-    try {
-      await client.get("/api/session/refresh");
-
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
-
   return {
     error,
     fetchData,
   };
+}
+
+async function refreshAccessToken(): Promise<boolean> {
+  try {
+    await client.get("/api/session/refresh");
+
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
