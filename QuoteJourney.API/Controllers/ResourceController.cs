47eed2a14f155f7519c6dcf2155292b6129ec4eb
Nick Hollas <nicholashollas@yahoo.co.uk>
2023-04-23 18:50:13 +0100

    Auth changes. Using cookies

---
+++ QuoteJourney.API/Controllers/ResourceController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuoteJourney.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class ActivityController : ApiController
+{
+    [HttpPost("Create", Name = "CreateActivity")]
+    public async Task<ActionResult<ActivityDto>> CreateActivity([FromBody] CreateActivityCommand command)
+    {
+        var createdActivity = await Sender.Send(command);
+
+        return CreatedAtAction(nameof(CreateActivity), createdActivity);
+    }
+
+    [HttpDelete("Delete/{activityId:guid}", Name = "DeleteActivity")]
+    public async Task<IActionResult> DeleteActivity(Guid activityId)
+    {
+        await Sender.Send(new DeleteActivityCommand(activityId));
+        return NoContent();
+    }
+
+    [HttpPut("Update", Name = "UpdateActivity")]
+    public async Task<IActionResult> UpdateActivity([FromBody] UpdateActivityCommand command)
+    {
+        await Sender.Send(command);
+        return NoContent();
+    }
+
+    [HttpPut("{activityId:guid}/LinkJob/{jobId:guid}", Name = "LinkJob")]
+    public async Task<IActionResult> LinkJob([FromRoute] Guid activityId, [FromRoute] Guid jobId)
+    {
+        var command = new LinkJobCommand(activityId, jobId);
+
+        await Sender.Send(command);
+        return NoContent();
+    }
+}
